11349596e5f4e9f4a083b84510bc8801bd4c967d
bhatiagagan24 <bhatiagagan24@gmail.com>
2021-09-24 12:09:17 +0530

    fetch weather data

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
 
-function App() {
+import React, {useEffect, useState} from 'react';
+
+
+
+export default function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => { navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+    await fetch('${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=&{long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}')
+    .then(res => res.json())
+    .then(result => {
+      setData(result)
+      console.log(result);
+    });
+  }
+    fetchData();
+    console.log('latitude is :- ', lat);
+    console.log('longitude is :- ', long);
+  }, [lat, long]);
+
+
   return (
     <div className="App">
 
     </div>
   );
 }
-
-export default App;
